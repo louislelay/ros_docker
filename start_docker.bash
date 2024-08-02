1c6c1851f06d702347e9f2e1a836d4128a73402c
Louis LE LAY <le.lay.louis@gmail.com>
2024-08-02 19:09:24 +0900

    Create start_docker.bash

---
+++ start_docker.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker run -it --rm \
+--env="DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+--env="LIBGL_ALWAYS_SOFTWARE=1" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--volume="$XAUTHORITY:/dot.Xauthority" \
+--mount type=bind,source="$(pwd)"/home,target=/home \
+--mount type=bind,source=/dev,target=/dev \
+--privileged \
+--net="host" \
+--name="ros_melodic_docker" \
+--workdir="/home" \
+ros_melodic_docker
